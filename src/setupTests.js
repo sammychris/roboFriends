bc41b2041c2ff30d4970f1654672846ae49a10a6
sammychris <ebusameric@gmail.com>
2022-02-23 19:59:40 +0100

    chore: Add a test setup

---
+++ src/setupTests.js
@@ -0,0 +1,9 @@
+// setup file
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import enableHooks from "jest-react-hooks-shallow";
+
+// pass an instance of jest to `enableHooks()`
+enableHooks(jest);
+
+configure({ adapter: new Adapter() });
