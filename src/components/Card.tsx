d363493a2ec6063d48aad08d20f6fadaca7dfdc6
sammychris <ebusameric@gmail.com>
2022-03-07 20:05:50 +0100

    feat: Add a Card to display robot

---
+++ src/components/Card.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface Icard {
+  name: string;
+  email: string;
+  id: number;
+}
+
+const Card: React.FC<Icard> = ({ name, email, id }) => {
+  return (
+    <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
+      <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
